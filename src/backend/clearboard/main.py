b62e4a38fd6f162608124e02985ae2c737495148
AladdinVR <adrien.berger@student-cs.fr>
2022-03-03 15:33:21 +0100

    updating main.py after code review and adding env vars management

--- src/backend/clearboard/main.py
+++ src/backend/clearboard/main.py
@@ -1,28 +1,30 @@
 """FastAPI main module for the Clearboard application.
-coreAddress : string, defines the part of the url that wont change between several jitsi-box
+core_address : string, defines the part of the url that wont change between several jitsi-box
 origins : string[], url to whitelist and on which the fastapi server should listen (basicly the core address)
 """
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from functools import lru_cache
+from . import config
 
 
 app = FastAPI()
-coreAddress = "http://localhost:3000/"
+@lru_cache()
+def get_settings():
+    return config.Settings()
 
-origins = [
-    "http://localhost:3000",
-]
-
+settings = get_settings()
+print(settings)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=settings.origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/getPolicy")
-def getPolicy(customAddress: str = "dty"):
-    """customAddress: string, part of the url that identify one meeting from another """
-    data = {"url": coreAddress + customAddress}
+@app.get("/policy")
+def get_policy(custom_address: str = "dty", settings: config.Settings = Depends(get_settings)):
+    """custom_address: string, part of the url that identify one meeting from another """
+    data = {"url": f"{settings.core_address}{custom_address}" }
     return data
