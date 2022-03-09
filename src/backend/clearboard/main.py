2107f14204e99256c2c7f10ae423358f9be7ab42
Fayssal Defaa <faydef@MacBook-Pro-de-Fayssal.local>
2022-03-09 11:22:02 +0100

    adds parallax processing

--- src/backend/clearboard/main.py
+++ src/backend/clearboard/main.py
@@ -2,6 +2,11 @@
 core_address : string, defines the part of the url that wont change between several jitsi-box
 origins : string[], url to whitelist and on which the fastapi server should listen (basicly the core address)
 """
+
+
+import cv2  # Import the OpenCV library
+
+
 from fastapi import FastAPI, Depends, Response, UploadFile, File, WebSocket, WebSocketDisconnect
 from websockets.exceptions import ConnectionClosedError
 from fastapi.middleware.cors import CORSMiddleware
@@ -25,6 +30,7 @@ from . import config
 
 app = FastAPI()
 
+
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
@@ -42,8 +48,10 @@ class ConnectionManager:
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
-            
+
+
 manager = ConnectionManager()
+
 
 @lru_cache()
 def get_settings():
@@ -82,17 +90,18 @@ async def post_picture(file: UploadFile = File(...)):
         return {"message": file.filename}
 
 
-             
 def image_to_base64(img: np.ndarray) -> bytes:
     """ Given a numpy 2D array, returns a JPEG image in base64 format """
 
     # using opencv 2, there are others ways
     img_buffer = imencode('.jpg', img)[1]
     return base64.b64encode(img_buffer).decode('utf-8')
-    
+
+
 def get_image(volume):
     image = volume
     return image_to_base64(image)
+
 
 @app.get("/photo")
 async def photo():
@@ -109,13 +118,13 @@ async def photo():
     except:
         print('no file to send')
 
-  
+
 @app.websocket("/ws1")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     temps = 0
     try:
-        while True: 
+        while True:
             await websocket.receive_text()
             if os.path.isfile(os.path.abspath('./clearboard/11.jpg')):
                 print('file found')
@@ -128,13 +137,56 @@ async def websocket_endpoint(websocket: WebSocket):
             else:
                 print('no file found')
                 time.sleep(1)
-               
+
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        
+
+
 class Item(BaseModel):
-    coord : list
-        
+    coord: list
+
+
 @app.post("/coord")
-async def post_coord(c : Item):
+async def post_coord(c: Item):
     print(c)
+
+    def traitement(imageNT, coordonnees):
+        def order_points(pts):
+            rect = np.zeros((4, 2), dtype="float32")
+            s = pts.sum(axis=1)
+            rect[0] = pts[np.argmin(s)]
+            rect[2] = pts[np.argmax(s)]
+            diff = np.diff(pts, axis=1)
+            rect[1] = pts[np.argmin(diff)]
+            rect[3] = pts[np.argmax(diff)]
+            return rect
+
+        def four_point_transform(image, pts):
+            rect = order_points(pts)
+            (tl, tr, br, bl) = rect
+            widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+            widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+            maxWidth = max(int(widthA), int(widthB))
+            heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+            heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+            maxHeight = max(int(heightA), int(heightB))
+            dst = np.array([
+                [0, 0],
+                [maxWidth - 1, 0],
+                [maxWidth - 1, maxHeight - 1],
+                [0, maxHeight - 1]], dtype="float32")
+            M = cv2.getPerspectiveTransform(rect, dst)
+            warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+            return warped
+
+        image = cv2.imread(imageNT)
+        cnt = np.array(coordonnees)
+        cropped = four_point_transform(image, cnt)
+
+        cv2.imwrite("cropped_reoriented.png", cropped)
+
+    try:
+        traitement('./clearboard/11.jpg', c)
+
+    except:
+        print('error occured')
