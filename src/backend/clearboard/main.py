1685bff56cfda51e95d87a3c970f78eb62536039
AladdinVR <adrien.berger@student-cs.fr>
2022-03-14 10:57:37 +0100

    implementing image process selection

--- src/backend/clearboard/main.py
+++ src/backend/clearboard/main.py
@@ -4,6 +4,7 @@ origins : string[], url to whitelist and on which the fastapi server should list
 """
 
 
+import string
 import cv2  # Import the OpenCV library
 from fastapi import FastAPI, Depends, Response, UploadFile, File, WebSocket, WebSocketDisconnect
 from websockets.exceptions import ConnectionClosedError
@@ -29,6 +30,7 @@ from . import config
 app = FastAPI()
 np.save('./coord.npy', None)
 img_cropped = None
+
 
 class ConnectionManager:
     def __init__(self):
@@ -101,20 +103,33 @@ def get_image(volume):
 
 
 @app.get("/photo")
-async def photo():
+async def photo(process=""):
     try:
         if os.path.exists(os.path.abspath('./cropped_reoriented.jpg')):
             img = Image.open('./cropped_reoriented.jpg')
             volume = np.asarray(img)
             image = get_image(volume)
             print('image sent')
+            if process == "process1":
+                print("process 1 selected")
+                return 'process 1 selected'
+            elif process == "process2":
+                print("process 2 selected")
+                return 'process 2 selected'
+            elif process == "process3":
+                print("process 3 selected")
+                return 'process 3 selected'
+            elif process == "process4":
+                print("process 4 selected")
+                return 'process 4 selected'
             return Response(content=image)
         else:
             print('file not found')
             time.sleep(1)
     except:
         print('no file to send')
-        
+
+
 @app.get("/original_photo")
 async def photo():
     try:
@@ -139,19 +154,20 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             await websocket.receive_text()
-            if (os.path.isfile(os.path.abspath('./clearboard/img_test.jpg') ) and (temps != os.path.getctime('./clearboard/img_test.jpg')) ):
-                    temps = os.path.getctime('./clearboard/img_test.jpg')
-                    temps_coord = os.path.getctime('./coord.npy')
-                    print('change file')
-                    try:
-                        c = np.load("./coord.npy")
-                    except:
-                        c = None
-                    traitement("./clearboard/img_test.jpg",c)
-                    print('new photo to send')
-                    
-                    await manager.send_personal_message("true", websocket)
-            
+            if (os.path.isfile(os.path.abspath('./clearboard/img_test.jpg')) and (temps != os.path.getctime('./clearboard/img_test.jpg'))):
+                temps = os.path.getctime('./clearboard/img_test.jpg')
+                temps_coord = os.path.getctime('./coord.npy')
+                print('change file')
+                try:
+                    c = np.load("./coord.npy")
+                except:
+                    c = None
+                traitement("./clearboard/img_test.jpg", c)
+
+                print('new photo to send')
+
+                await manager.send_personal_message("true", websocket)
+
             elif (os.path.isfile(os.path.abspath('./coord.npy'))) and (temps_coord != os.path.getctime('./coord.npy')):
                 temps_coord = os.path.getctime('./coord.npy')
                 print('change of coordinates')
@@ -159,11 +175,11 @@ async def websocket_endpoint(websocket: WebSocket):
                     c = np.load("./coord.npy")
                 except:
                     c = None
-                traitement("./clearboard/img_test.jpg",c)
+                traitement("./clearboard/img_test.jpg", c)
                 print('new photo to send')
-                
+
                 await manager.send_personal_message("true", websocket)
-                
+
             else:
                 await manager.send_personal_message("false", websocket)
 
@@ -175,58 +191,59 @@ async def websocket_endpoint(websocket: WebSocket):
 class Coordinates(BaseModel):
     coord: List[List[str]] = []
 
+
 def traitement(imageNT, coordonnees):
-        def order_points(pts):
-            rect = np.zeros((4, 2), dtype="float32")
-            s = pts.sum(axis=1)
-            rect[0] = pts[np.argmin(s)]
-            rect[2] = pts[np.argmax(s)]
-            diff = np.diff(pts, axis=1)
-            rect[1] = pts[np.argmin(diff)]
-            rect[3] = pts[np.argmax(diff)]
-            return rect
+    def order_points(pts):
+        rect = np.zeros((4, 2), dtype="float32")
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        diff = np.diff(pts, axis=1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        return rect
 
-        def four_point_transform(image, pts):
-            rect = order_points(pts)
-            (tl, tr, br, bl) = rect
-            widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-            widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-            maxWidth = max(int(widthA), int(widthB))
-            heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-            heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-            maxHeight = max(int(heightA), int(heightB))
-            dst = np.array([
-                [0, 0],
-                [maxWidth - 1, 0],
-                [maxWidth - 1, maxHeight - 1],
-                [0, maxHeight - 1]], dtype="float32")
-            M = cv2.getPerspectiveTransform(rect, dst)
-            warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-            return warped
+    def four_point_transform(image, pts):
+        rect = order_points(pts)
+        (tl, tr, br, bl) = rect
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype="float32")
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+        return warped
 
-        image = cv2.imread(imageNT)
-        cropped = image.copy()
-        try:
-            cnt = np.array(coordonnees)
-            cropped = four_point_transform(image, cnt)
-        except:
-            pass
-        
-        start = time.time()
-        cv2.imwrite("./cropped_reoriented.jpg", cropped)
-        end =  time.time()
-        print('temps imwrite save', end - start)
+    image = cv2.imread(imageNT)
+    cropped = image.copy()
+    try:
+        cnt = np.array(coordonnees)
+        cropped = four_point_transform(image, cnt)
+    except:
+        pass
+
+    start = time.time()
+    cv2.imwrite("./cropped_reoriented.jpg", cropped)
+    end = time.time()
+    print('temps imwrite save', end - start)
 
 
 @app.post("/coord")
 async def post_coord(coordinates: Coordinates):
     c = coordinates.coord
-    co = [ [int(float(k[0])), int(float(k[1]))] for k in c]
+    co = [[int(float(k[0])), int(float(k[1]))] for k in c]
     start = time.time()
     np.save('./coord.npy', co)
-    end =  time.time()
+    end = time.time()
     print('temps np save', end - start)
-    
+
     try:
         start = time.time()
         traitement('./clearboard/img_test.jpg', co)
