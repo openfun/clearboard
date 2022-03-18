1fb20cdf5a533d2f30b1c97848b114620f10430c
AladdinVR <adrien.berger@student-cs.fr>
2022-03-18 11:06:52 +0100

    🎨(lambda) fix linter error
    Fix linter issues to pass CI

--- src/terraform/lambda-enhance/app.py
+++ src/terraform/lambda-enhance/app.py
@@ -4,10 +4,13 @@ import boto3
 s3_client = boto3.client("s3")
 
 # pylint: disable=unused-argument
+
+
 def handler(event, context):
     """
     Triggered by S3 each time an image is uploaded to the S3 source bucket.
-    Enhance image to extract black board and publish the result to S3 destination bucket
+    Enhance image to extract black board 
+    and publish the result to S3 destination bucket
     """
     for record in event["Records"]:
         source_bucket_name = record["s3"]["bucket"]["name"]
@@ -17,5 +20,7 @@ def handler(event, context):
             "Key": key,
         }
         s3_client.copy(
-            copy_source, source_bucket_name.replace("source", "destination"), key
+            copy_source,
+            source_bucket_name.replace("source", "destination"),
+            key
         )
