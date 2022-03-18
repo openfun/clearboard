183234078892e02f1c6aa926f9ea31a4b57d3786
djexvr <ruhlmannvianney@gmail.com>
2022-03-18 15:19:54 +0100

    🩹(lambda) fix flake8 error
    Fix a lint error due to a bad configuration of flake8. Since lambda is
    deprecated and will be removed in a later version there is no need to
    configure it and this simple fix should be enough.

--- src/terraform/lambda-enhance/app.py
+++ src/terraform/lambda-enhance/app.py
@@ -9,18 +9,14 @@ s3_client = boto3.client("s3")
 def handler(event, context):
     """
     Triggered by S3 each time an image is uploaded to the S3 source bucket.
-    Enhance image to extract black board 
+    Enhance image to extract black board
     and publish the result to S3 destination bucket
     """
     for record in event["Records"]:
-        source_bucket_name = record["s3"]["bucket"]["name"]
+        s3_name = record["s3"]["bucket"]["name"]
         key = record["s3"]["object"]["key"]
         copy_source = {
-            "Bucket": source_bucket_name,
+            "Bucket": s3_name,
             "Key": key,
         }
-        s3_client.copy(
-            copy_source,
-            source_bucket_name.replace("source", "destination"),
-            key
-        )
+        s3_client.copy(copy_source, s3_name.replace("source", "dest"), key)
