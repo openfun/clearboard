e4e046a46ef820f5c71c6b6cb8a86c868cf70ab5
AladdinVR <adrien.berger@student-cs.fr>
2022-03-23 16:45:23 +0100

    ⚗️(env) test to give env vars
    test to give and apply env vars to make .env usefull

--- src/backend/clearboard/config.py
+++ src/backend/clearboard/config.py
@@ -7,14 +7,11 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     """Define fastapi settings"""
 
-    ORIGINS: typing.List = []
+    ORIGINS: str
     MEDIA_ROOT: str
 
     class Config:
         """Define fastapi env configuration"""
 
-        env_prefix = ""
-        env_file = "../../env.d/development.dist"
-        env_file_encoding = "utf-8"
         arbitrary_types_allowed = True
         case_sensitive = False
