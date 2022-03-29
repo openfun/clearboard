44b327dfae76d4c3b9a823102d77f6e5233f1460
AladdinVR <adrien.berger@student-cs.fr>
2022-03-29 16:25:48 +0200

    ⚡️(black_n_white) improve black and white rendering
    improve black and white rendering and remove commented
    code to improve efficiency of filter for Lyon

--- src/backend/clearboard/black_n_white.py
+++ src/backend/clearboard/black_n_white.py
@@ -5,23 +5,18 @@ import numpy as np
 
 def black_n_white(name, dest):
     """process image on path name to black and white and output in dest"""
-    gamma = 0.4
+    gamma = 0.8
+
     img_original = cv2.imread(name, 0)
-    look_up_table = np.empty((1, 256), np.uint8)
-    for i in range(256):
-        look_up_table[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
-
+    values = np.arange(0, 256)
+    look_up_table = np.uint8(255 * np.power((values/255.0), gamma))
     res = cv2.LUT(img_original, look_up_table)
-    # res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-    # T = threshold_local(res, 11, offset=10, method="gaussian")
-    # res = (res > T).astype("uint8") * 255
-    # [changing-contrast-brightness-gamma-correction]
 
-    clahe = cv2.createCLAHE().apply(res)
+    clahe = cv2.createCLAHE(clipLimit=0.5, tileGridSize=(16, 16)).apply(res)
 
     sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-    sharpen = cv2.filter2D(clahe, -1, sharpen_kernel)
+    sharpen = cv2.filter2D(res, -1, sharpen_kernel)
 
-    thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
+    thresh = cv2.adaptiveThreshold(clahe, 190, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                   cv2.THRESH_BINARY, 11, 12)
     cv2.imwrite(dest, thresh)
